e9ea707541004214f425cce8561b48a4581db8f2
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-03-02 13:09:16 +0000

    fix app name

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i-think-therefore-i-blog-jm.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['i-think-therefore-i-blog-jm.herokuapp.com', 'localhost']
 
 
 # Application definition
