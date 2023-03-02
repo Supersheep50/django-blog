441c079d38a1d2201d23ae74920fcd062a92f2eb
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-03-02 14:56:01 +0000

    fix secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-import os
+
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
